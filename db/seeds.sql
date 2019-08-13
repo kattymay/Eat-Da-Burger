4c0ad101a2d455456b11dfac38574f83d0cc4578
Katherine Walters <kattymay222@gmail.com>
2019-08-13 09:56:39 -0400

    first commit to sdat to save

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Sweet Potato');
+INSERT INTO burgers (name) VALUES ('Lentil');
+INSERT INTO burgers (name, devoured) VALUES ('Chickpea', true);
+INSERT INTO burgers (name, devoured) VALUES ('Soy', true);
+INSERT INTO burgers (name, devoured) VALUES ('Black Bean', true);
