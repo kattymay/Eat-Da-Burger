05eb0a78351382c7abd6cc97fd341c15bf6434f8
Katherine Walters <kattymay222@gmail.com>
2019-08-14 16:27:40 -0400

    updated with correct port

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
-// Set up MySQL connection.
+// sets up MySQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+// Heroku jawsdb_url
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+  var connection = mysql.createConnection({
   host: "localhost",
   port: 3000,
   user: "root",
   password: "root",
   database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,11 @@
-INSERT INTO burgers (name) VALUES ('Sweet Potato');
-INSERT INTO burgers (name) VALUES ('Lentil');
-INSERT INTO burgers (name, devoured) VALUES ('Chickpea', true);
-INSERT INTO burgers (name, devoured) VALUES ('Soy', true);
-INSERT INTO burgers (name, devoured) VALUES ('Black Bean', true);
+INSERT INTO burgers (name, devoured) 
+VALUES ('Lentil Burger', false);
+
+INSERT INTO burgers (name, devoured)
+VALUES ('Chickpea Burger', true);
+
+INSERT INTO burgers (name, devoured) 
+VALUES ('Soy Burger', false);
+
+INSERT INTO burgers (name, devoured) 
+VALUES ('Black Bean Burger', true);
