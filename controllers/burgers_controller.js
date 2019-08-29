1f00c037d4dab29513ca55ead2baa2fc540f2308
Katherine Walters <kattymay222@gmail.com>
2019-08-28 20:40:18 -0400

    cleaner code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,45 +6,36 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    res.render("index", {burgers : data});
+router.get("/", function (req, res) {
+  burger.all(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject)
+    res.render("index", hbsObject);
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "name", "devoured"
-  ], [
-    req.body.name, req.body.data-id
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+router.post("/api/burgers", function (req, res) {
+  burger.create("burger_name", [req.body.name], function (result) {
+    res.json(true)
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
   var devoured = "devoured = " + true;
+  console.log("condition", condition);
 
-  burger.update(devoured, condition, function(result){
+  burger.update(devoured, condition, function (result) {
     if (result.changedRows === 0) {
-        return res.status(404).end();
+      return res.status(404).end();
     } else {
-        res.status(200).end();
+      res.status(200).end();
     }
-});
+  });
 });
 
-router.delete("/api/burgers", function(req, res){
-burger.delete(function(result){
-  if (result.affectedRows == 0) {
-    return res.status(404).end();
-  } else {
-    res.status(200).end();
-  }
-});
-})
 
 // Export routes for server.js to use.
 module.exports = router;
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,38 @@
+$(function() {
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+      console.log("create form submitted");
+  
+      var newBurger = {
+        name: $("#burger")
+          .val()
+          .trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(function() {
+        console.log("Your order is ready", newBurger);
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    });
+  
+    $(".eat").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      console.log(id);
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT"
+      }).then(function() {
+        console.log("changed burger to", devoured);
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    });
+  });
